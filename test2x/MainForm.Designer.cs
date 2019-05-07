36a3870063921da9a106666201e546583ea045a1
TakashiNord <evg.gal@live.ru>
2019-05-07 11:49:55 +0300

    Приложение Банкомат (ввод и выдача)
    2. Краткое описание работы.
    
      2.1 запустите приложение
      2.2 Выберите режим работы банкомата
          кнопка 'Выдача' - выдача денег
          кнопка 'Прием денег' - "принимает" в банкомат деньги.
      2.3 Режим Выдача.
    
          1. Нажимая кнопки '50\100\500\1000 рублей'
             банкомат выдает купюры и уменьшает общее количество денег у него.
          2. Если купюры данного номинала иссякнут - будет выдано сообщение.
          3. Нажимая кнопку 'Другая сумма'
             окно ввода Суммы становиться активным.
             В данное окно вы можете ввести необходимую сумму для выдачи.
             Также можете указать, типы купюр для выдачи суммы.
             Далее, нажимая кнопку 'Принять'
             - программа "выдает требуемую сумму" и уменьшает общее количество денег
               в Банке.
    
      2.4 Режим 'Прием денег'.
    
          1. Нажимая кнопки '50\100\500\1000 рублей'
             банкомат принимает купюры, увеличивая общее количество денег у себя.
          2. В случае, переполнения "кубышки", он откажется принимать купюру.

---
+++ test2x/MainForm.Designer.cs
@@ -0,0 +1,289 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: gal
+ * Date: 21.06.2016
+ * Time: 16:28
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace test2x
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label50;
+		private System.Windows.Forms.Label label100;
+		private System.Windows.Forms.Label label500;
+		private System.Windows.Forms.Label label1000;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label50 = new System.Windows.Forms.Label();
+			this.label100 = new System.Windows.Forms.Label();
+			this.label500 = new System.Windows.Forms.Label();
+			this.label1000 = new System.Windows.Forms.Label();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(230, 117);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(127, 27);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Банкомат";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(60, 159);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(166, 44);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "50  рублей";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(61, 223);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(165, 46);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "100 рублей";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(359, 160);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(149, 43);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "500 рублей";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(357, 224);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(153, 45);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "1000 рублей";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(167, 390);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(78, 23);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Сумма=";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(251, 390);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(114, 26);
+			this.textBox1.TabIndex = 5;
+			this.textBox1.Enter += new System.EventHandler(this.TextBox1Enter);
+			// 
+			// label50
+			// 
+			this.label50.Location = new System.Drawing.Point(12, 133);
+			this.label50.Name = "label50";
+			this.label50.Size = new System.Drawing.Size(109, 23);
+			this.label50.TabIndex = 7;
+			this.label50.Text = "label50";
+			// 
+			// label100
+			// 
+			this.label100.Location = new System.Drawing.Point(13, 277);
+			this.label100.Name = "label100";
+			this.label100.Size = new System.Drawing.Size(107, 21);
+			this.label100.TabIndex = 8;
+			this.label100.Text = "label100";
+			// 
+			// label500
+			// 
+			this.label500.Location = new System.Drawing.Point(508, 137);
+			this.label500.Name = "label500";
+			this.label500.Size = new System.Drawing.Size(100, 23);
+			this.label500.TabIndex = 9;
+			this.label500.Text = "label500";
+			// 
+			// label1000
+			// 
+			this.label1000.Location = new System.Drawing.Point(508, 277);
+			this.label1000.Name = "label1000";
+			this.label1000.Size = new System.Drawing.Size(100, 23);
+			this.label1000.TabIndex = 10;
+			this.label1000.Text = "label1000";
+			// 
+			// button5
+			// 
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button5.Location = new System.Drawing.Point(597, 86);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(131, 75);
+			this.button5.TabIndex = 11;
+			this.button5.Text = "Выдача";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// button6
+			// 
+			this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button6.Location = new System.Drawing.Point(597, 169);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(131, 85);
+			this.button6.TabIndex = 12;
+			this.button6.Text = "Прием денег";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.Button6Click);
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.ForeColor = System.Drawing.Color.Red;
+			this.label3.Location = new System.Drawing.Point(60, 19);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(450, 67);
+			this.label3.TabIndex = 13;
+			this.label3.Text = "Режим - Выдачи денег";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.ForeColor = System.Drawing.Color.Green;
+			this.label4.Location = new System.Drawing.Point(597, 19);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(131, 58);
+			this.label4.TabIndex = 14;
+			this.label4.Text = "Выберите режим работы Банкомата ";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(230, 277);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(119, 44);
+			this.button7.TabIndex = 15;
+			this.button7.Text = "Другая сумма";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.Button7Click);
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(371, 390);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(92, 23);
+			this.button8.TabIndex = 16;
+			this.button8.Text = "Принять";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.Button8Click);
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.CheckOnClick = true;
+			this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Items.AddRange(new object[] {
+			"50 рубл",
+			"100 рубл",
+			"500 рубл",
+			"1000 рубл"});
+			this.checkedListBox1.Location = new System.Drawing.Point(496, 373);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(138, 88);
+			this.checkedListBox1.TabIndex = 17;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(740, 490);
+			this.Controls.Add(this.checkedListBox1);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.label1000);
+			this.Controls.Add(this.label500);
+			this.Controls.Add(this.label100);
+			this.Controls.Add(this.label50);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "MainForm";
+			this.Text = "test2x";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ test2x/MainForm.cs
@@ -0,0 +1,324 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: gal
+ * Date: 21.06.2016
+ * Time: 16:28
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace test2x
+{
+
+
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		
+		
+	/// <summary>
+	/// Description of Bank1.
+	/// </summary>
+	public class Bank
+	{
+		// максимальное количество купюр хранимых в Банке.
+		// 50 - 100 - 500 - 1000
+		public int[] SummaMax={50,30,20,10};
+		
+		// текущее количество количество купюр хранимых в Банке.
+		// 50 - 100 - 500 - 1000		
+		public int[] SummaCurr={-1,-1,-1,-1};
+		
+		public Bank()
+		{
+		}
+		
+		public Bank(int cnt50, int cnt100, int cnt500 , int cnt1000)
+		{
+			this.SummaCurr[0]=cnt50;
+			this.SummaCurr[1]=cnt100;
+			this.SummaCurr[2]=cnt500;
+			this.SummaCurr[3]=cnt1000;
+
+			this.SummaMax[0]=cnt50;
+			this.SummaMax[1]=cnt100;
+			this.SummaMax[2]=cnt500;
+			this.SummaMax[3]=cnt1000;			
+		}		
+		
+		// i - индекс 
+		// flag - флаг , true - добавляем
+        //               false - снимаем 		
+		public int IncrDecr(int i, bool flag)
+		{
+			if (i<0 || i>3) return(-1) ;
+			if (flag) {
+				if (this.SummaCurr[i]<SummaMax[i]) { this.SummaCurr[i]=this.SummaCurr[i]+1; }
+				else {
+				 string message = "Вы превысили лимит по числу купюр.\n Купюра не принята.";
+                 string caption = "Системное...";
+                 MessageBox.Show(message, caption,
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Stop);
+                 return(-2);
+				}
+			} else {
+				if (this.SummaCurr[i]>0) { this.SummaCurr[i]=this.SummaCurr[i]-1; }
+				else {
+				 string message = "Купюр данного номинала больше нет.\n Попробуйте в другой раз.";
+                 string caption = "Системное...";
+                 MessageBox.Show(message, caption,
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Stop);
+                 return(-3);
+				}				
+				
+			}
+			return(0);
+		}		
+		
+		// i - индекс 
+        public int GetCurr(int i)
+		{
+			if (i<0 || i>3) return(-1) ;
+			return(this.SummaCurr[i]);
+		}
+		
+		
+	}		
+		
+
+		
+	    int mode = 1 ; // 1-выдача, 2 - прием;
+		
+		Bank b1 = new Bank(50,30,20,10);
+		
+		void Button0Click(int i, string m)
+		{
+			textBox1.Text="0";
+			textBox1.ReadOnly=true;
+			button8.Visible=false;
+			checkedListBox1.Visible=false;
+			if (mode==1) {
+				if (0==b1.IncrDecr(i, false)) {
+					textBox1.Text=m;
+				}
+			}
+			else {
+				if (0==b1.IncrDecr(i, true)) {
+					textBox1.Text=m;
+				}
+			}
+		}
+		
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+			label50.Text=b1.GetCurr(0).ToString() + " купюр";
+			label100.Text=b1.GetCurr(1).ToString() + " купюр";
+			label500.Text=b1.GetCurr(2).ToString() + " купюр";
+			label1000.Text=b1.GetCurr(3).ToString() + " купюр";
+			checkedListBox1.SetItemChecked(0,true);
+			checkedListBox1.SetItemChecked(1,true);
+			checkedListBox1.SetItemChecked(2,true);
+			checkedListBox1.SetItemChecked(3,true);
+			Button5Click(sender, e);
+	
+		} 
+		void Button1Click(object sender, EventArgs e)
+		{
+			Button0Click(0, "50");
+			label50.Text=b1.GetCurr(0).ToString() + " купюр";
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			Button0Click(1, "100");
+			label100.Text=b1.GetCurr(1).ToString() + " купюр";	
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			Button0Click(2, "500");
+			label500.Text=b1.GetCurr(2).ToString() + " купюр";	
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+			Button0Click(3, "1000");
+			label1000.Text=b1.GetCurr(3).ToString() + " купюр";	
+		}
+		void Button5Click(object sender, EventArgs e)
+		{
+			mode=1;
+			label3.Text="Режим - Выдача денег";
+			button7.Visible=true;
+			button8.Visible=false;
+			textBox1.ReadOnly=true;
+			textBox1.Text="";
+			checkedListBox1.Visible=false;
+		}
+		void Button6Click(object sender, EventArgs e)
+		{
+			mode=2;
+			label3.Text="Режим - Приема денег";
+			button7.Visible=false;
+			button8.Visible=false;
+			textBox1.ReadOnly=true;
+			textBox1.Text="";
+			checkedListBox1.Visible=false;
+		}
+		void Button7Click(object sender, EventArgs e)
+		{
+			textBox1.Text="";
+			textBox1.ReadOnly=false;
+			button8.Visible=true;
+			checkedListBox1.Visible=true;
+
+		}
+		void Button8Click(object sender, EventArgs e)
+		{
+			string message;
+			string caption;
+			
+			string s=textBox1.Text;
+			int numValue1=0;
+            if (!Int32.TryParse(s, out numValue1)) s="0";
+            if (numValue1<=0 || numValue1%50!=0) {
+ 				 message = "Вводимая сумма должна быть кратна 50 руб.";
+                 caption = "Системное...";
+                 MessageBox.Show(message, caption,
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Stop);  
+                 return ;
+            }
+            textBox1.Text=s;
+            
+            // Пользователь не отметил ни одну из банкнот
+            if (checkedListBox1.GetItemChecked(3)==false &&
+                checkedListBox1.GetItemChecked(2)==false &&
+                checkedListBox1.GetItemChecked(1)==false &&
+                checkedListBox1.GetItemChecked(0)==false) return ;
+   
+            int sum=numValue1;
+            // количество денег для выдачи - 50, 100, 500, 1000
+            int [] cntMoney={0,0,0,0};
+
+            // если сумма больше 1000 и пользователь согласился с выдачей 
+            if (sum>=1000 && checkedListBox1.GetItemChecked(3)==true) {
+            	// проверяем если в Банке купюры
+            	int cnt=b1.GetCurr(3);
+            	if (cnt>0) {
+            		while (sum>=1000 && cnt>0) {
+            			cnt=cnt-1;
+            			cntMoney[3]=cntMoney[3]+1;
+            			sum=sum-1000;
+            		}
+            	}
+            }
+
+            // если сумма больше 500 и пользователь согласился с выдачей 
+            if (sum>=500 && checkedListBox1.GetItemChecked(2)==true) {
+            	// проверяем если в Банке купюры
+            	int cnt=b1.GetCurr(2);
+            	if (cnt>0) {
+            		while (sum>=500 && cnt>0) {
+            			cnt=cnt-1;
+            			cntMoney[2]=cntMoney[2]+1;
+            			sum=sum-500;
+            		}
+            	}
+            }            
+
+            // если сумма больше 100 и пользователь согласился с выдачей 
+            if (sum>=100 && checkedListBox1.GetItemChecked(1)==true) {
+            	// проверяем если в Банке купюры
+            	int cnt=b1.GetCurr(1);
+            	if (cnt>0) {
+            		while (sum>=100 && cnt>0) {
+            			cnt=cnt-1;
+            			cntMoney[1]=cntMoney[1]+1;
+            			sum=sum-100;
+            		}
+            	}
+            }            
+
+            // если сумма больше 100 и пользователь согласился с выдачей 
+            if (sum>=50 && checkedListBox1.GetItemChecked(0)==true) {
+            	// проверяем если в Банке купюры
+            	int cnt=b1.GetCurr(0);
+            	if (cnt>0) {
+            		while (sum>=50 && cnt>0) {
+            			cnt=cnt-1;
+            			cntMoney[0]=cntMoney[0]+1;
+            			sum=sum-50;
+            		}
+            	}
+            } 
+
+            if (sum>0) {
+              message = "В банкомате недостаточно средств для выдачи или нет необходимых банкнот.";
+              message = message + "\n Обратитесь позже или измените запрашиваемую сумму.";
+              message = message + "\n Не хватает до требуемой суммы = " +  sum.ToString() + " рубл.";
+              caption = "Системное...";
+              MessageBox.Show(message, caption,
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Asterisk); 
+              return ;
+            }
+
+            message = "Количество выдаваемых банкнот:";
+            if (cntMoney[3]>0) message = message + "\n\t 1000 рубл = " + cntMoney[3].ToString() + " шт.";
+            if (cntMoney[2]>0) message = message + "\n\t  500 рубл = " + cntMoney[2].ToString() + " шт.";
+            if (cntMoney[1]>0) message = message + "\n\t  100 рубл = " + cntMoney[1].ToString() + " шт.";
+            if (cntMoney[0]>0) message = message + "\n\t   50 рубл = " + cntMoney[0].ToString() + " шт.";
+            message = message + "\n\t  -----------------";
+            message = message + "\n\t  Сумма = " + numValue1.ToString() + " рубл";
+            caption = "Системное...";
+            MessageBox.Show(message, caption,
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+
+            // Списываем деньги 
+            for (int j=0;j<cntMoney.Length; j++) {
+            	while (cntMoney[j]>0) {
+            		if (j==0) Button0Click(0, "50");
+            		if (j==1) Button0Click(1, "100");
+            		if (j==2) Button0Click(2, "500");
+            		if (j==3) Button0Click(3, "1000");
+            		cntMoney[j]=cntMoney[j]-1;
+            	}
+            }
+            
+			label50.Text=b1.GetCurr(0).ToString() + " купюр";
+			label100.Text=b1.GetCurr(1).ToString() + " купюр";	
+			label500.Text=b1.GetCurr(2).ToString() + " купюр";	
+			label1000.Text=b1.GetCurr(3).ToString() + " купюр";	
+            
+		}
+		void TextBox1Enter(object sender, EventArgs e)
+		{
+	        //Button8Click(sender, e);
+		}
+	
+
+	}
+}
